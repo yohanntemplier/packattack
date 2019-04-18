eb5555c1d03e7d7f927251f4e91182ba30c7503d
Jessica Salvat <jsalvat7@gmail.com>
2019-04-18 23:50:46 +0200

    jkjd

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -13,11 +13,11 @@ class GameController extends AbstractController
         $mechants = [];
         $oeufs = [];
         $itemManager = new GameManager();
-        for ($i = 0; $i < 80; $i++) {
+        for ($i = 0; $i < 40; $i++) {
             $oeuf = $itemManager->oeufhasard();
             $oeufs[] = $oeuf;
         }
-        for ($i = 0; $i < 40; $i++) {
+        for ($i = 0; $i < 15; $i++) {
             $mechant = $itemManager->mechanthasard();
             $mechants[] = $mechant;
         }
@@ -25,5 +25,24 @@ class GameController extends AbstractController
         shuffle($items);
 
         return $this->twig->render('Game/Levelone.html.twig', ['items' => $items]);
+    }
+
+    public function leveltwo()
+    {
+        $mechants = [];
+        $oeufs = [];
+        $itemManager = new GameManager();
+        for ($i = 0; $i < 65; $i++) {
+            $oeuf = $itemManager->oeufhasard();
+            $oeufs[] = $oeuf;
+        }
+        for ($i = 0; $i < 30; $i++) {
+            $mechant = $itemManager->mechanthasard();
+            $mechants[] = $mechant;
+        }
+        $items = array_merge($oeufs, $mechants);
+        shuffle($items);
+
+        return $this->twig->render('Game/Leveltwo.html.twig', ['items' => $items]);
     }
 }
