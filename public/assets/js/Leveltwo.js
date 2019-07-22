7d887e10057c0174589b23fb811b27274007ed16
yohanntemplier <templieryohann@gmail.com>
2019-07-22 09:42:06 +0200

    modif repo

--- public/assets/js/Leveltwo.js
+++ public/assets/js/Leveltwo.js
@@ -5,7 +5,7 @@ function decompte() {
     compteur -= 1;
     document.getElementById('compteur').innerHTML = compteur;
 
-    if (compteur <= 2) {
+    if (compteur == 2) {
         document.location.href = "../../Game/GameOver";
     }
     setTimeout(decompte, 1000);
